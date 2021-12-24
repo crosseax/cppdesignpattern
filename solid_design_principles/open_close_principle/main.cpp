edf9c5fb9878cf6a6ce6c71b8c8c3d85e94261c0
crosseax <qq417737572@gmail.com>
2021-12-25 01:50:23 +0800

    better implementation for one specification

--- solid_design_principles/open_close_principle/main.cpp
+++ solid_design_principles/open_close_principle/main.cpp
@@ -19,7 +19,7 @@ struct ProductFilter {
     // This works, but doesnt scale
     // if have n criteria, then 2^n-1 functions
     // we are modifying existing code, and this isn't great
-    
+
     std::vector<Product*> by_color(std::vector<Product*> items, Color color) {
         std::vector<Product*> result;
         for (auto& i : items) {
@@ -51,7 +51,35 @@ struct ProductFilter {
     }
 };
 
+template <typename T> 
+struct Specification {
+    virtual bool is_satisfied(T* item) = 0;
+};
 
+template <typename T> 
+struct Filter {
+    virtual std::vector<T*> filter(std::vector<T*> items, Specification<T>& spec) = 0;
+};
+
+struct BetterFilter : Filter<Product> {
+    std::vector<Product*> filter(std::vector<Product*> items, Specification<Product>& spec) override {
+        std::vector<Product*> result;
+        for (auto& item : items) {
+            if (spec.is_satisfied(item)) {
+                result.push_back(item);
+            }
+        }
+        return result;
+    }
+};
+
+struct ColorSpecification : Specification<Product> {
+    Color color;
+    ColorSpecification(Color color) : color {color} {}
+    bool is_satisfied(Product* item) override {
+        return item->color == color;
+    }
+};
 
 int main (void)
 {
@@ -60,11 +88,21 @@ int main (void)
     Product house {"House", Color::blue, Size::large};
 
     std::vector<Product*> items { &apple, &tree, &house };
+    
     ProductFilter pf;
     std::vector<Product*> green_things = pf.by_color(items, Color::green);
 
     for (auto& item : green_things) {
         std::cout << item->name << " is green" << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    BetterFilter bf;
+    ColorSpecification green (Color::green);
+    
+    for (auto& item : bf.filter(items, green)) {
+        std::cout << item->name << " is green using better filter" << std::endl;
     }
 
     return 0;
