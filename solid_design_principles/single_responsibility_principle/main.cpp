26bac2664b8e8721dfd7ec90957bfd3d75579f9c
crosseax <qq417737572@gmail.com>
2021-12-24 22:41:45 +0800

    better design using by seperate out the different types of concern

--- solid_design_principles/single_responsibility_principle/main.cpp
+++ solid_design_principles/single_responsibility_principle/main.cpp
@@ -20,21 +20,46 @@ struct Journal {
         entries.push_back(boost::lexical_cast<std::string>(count++) + "; " + entry);
     }
 
-    void save(const std::string& filename) {
+    // // This is a NOT good implementation
+    // // Because what if you wanna do save to others
+    // // or do change,
+    // // then you have to change every single one of them
+    // void save(const std::string& filename) {
+    //     std::ofstream ofs (filename);
+    //     for (auto& e : entries) {
+    //         ofs << e << std::endl;
+    //     }
+    // }
+};
+
+// Seperation of concern
+// As Persistence Manager grow
+// You have all that utility codes in a single place
+struct PersistenceManager {
+    static void save(const Journal& journal, const std::string& filename) {
         std::ofstream ofs (filename);
-        for (auto& e : entries) {
+        for (auto& e : journal.entries) {
             ofs << e << std::endl;
         }
     }
-
 };
 
+// This is a gist of single responsibility principle
+// The journal is responsible for its own title and entries 
+// and the way you work with them,
+// But when it comes to persistence, 
+// it makes more sense to seperate out this concern
 
 int main (void) 
 {
     Journal journal{"Dear Diary"};
     journal.add_entry("I ate a bug");
     journal.add_entry("I laughed today");
+
+    // Journal.save("diary.txt")
+
+    PersistenceManager pm;
+    pm.save(journal, "diary.txt");
 
     return 0;
 }
