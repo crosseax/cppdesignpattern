297f31037e92cfc3ee14dcddcb698352a68013f7
crosseax <qq417737572@gmail.com>
2021-12-27 01:36:25 +0800

    now we good cuz we depend on some abstraction instead of concrete classes

--- solid_design_principles/dependency_inversion_pinciple/main.cpp
+++ solid_design_principles/dependency_inversion_pinciple/main.cpp
@@ -25,38 +25,70 @@ struct Person {
     std::string name;
 };
 
+// We have two ideas
+// moving something to a low-level module
+// and intdoducing an abstraction
+
+// this is the abstraction
+// it provides few useful funcitonality for searching 
+struct RelationshipBrowser {
+    virtual std::vector<Person> find_all_children_of(const std::string& name) = 0;
+};
+
+
 // The struct below, Relationships
 // is typically called:
 // Low-level module/construct 
 // because what it does essentially is 
 // to provide functionality for data storage
 // and a couple of utility functions to populate that storage
-struct Relationships { 
+struct Relationships : RelationshipBrowser { 
     std::vector<std::tuple<Person, Relationship, Person>> relations;
 
     void add_parent_and_child(const Person& parent, const Person& child) {
         relations.push_back({parent, Relationship::parent, child});
         relations.push_back({child, Relationship::child, parent});
     }
+
+    // now can implement
+    std::vector<Person> find_all_children_of (const std::string& name) override {
+        std::vector<Person> result;
+        for (auto&& [first, rel, second] : relations) {
+            if (first.name == name && rel == Relationship::parent) {
+                result.push_back(second);
+            }
+        }
+        return result;
+    }
 };
 
 // if you want to perform a research on a data,
 // this will be called a High-level module
 struct Research {
-    // below is the worst thing to do
-    // because it violates point A above
-    Research(Relationships& relationships) {
-        // and it's accessing the low-level module's data
-        auto& relations = relationships.relations;
-        for (auto&& [first, rel, second] : relations) { // for C++17
-            if (first.name == "John" && rel == Relationship::parent) {
-                std::cout << "John has a child called " << second.name << std::endl;
-            }
+    // // below is the worst thing to do
+    // // because it violates point A above
+    // Research(Relationships& relationships) {
+    //     // and it's accessing the low-level module's data
+    //     auto& relations = relationships.relations;
+    //     for (auto&& [first, rel, second] : relations) { // for C++17
+    //         if (first.name == "John" && rel == Relationship::parent) {
+    //             std::cout << "John has a child called " << second.name << std::endl;
+    //         }
+    //     }
+    // }
+    // // if the low-level decides to change storage
+    // // like hiding the relations by making it private
+    // // then this high level module would be completely broken
+
+    
+    // Now can rewrite the research component
+    // instead of depending on details,
+    // now just to depend on a relationship browser
+    Research(RelationshipBrowser& browser) {
+        for (auto& child : browser.find_all_children_of("John")) {
+            std::cout << "John has a child called " << child.name << std::endl; 
         }
     }
-    // if the low-level decides to change storage
-    // like hiding the relations by making it private
-    // then this high level module would be completely broken
 };
 
 
@@ -69,6 +101,7 @@ int main (void)
     relationships.add_parent_and_child(parent, child1);
     relationships.add_parent_and_child(parent, child2);
 
+    // no need to change any code here in main
     Research _(relationships);
 
     return 0;
