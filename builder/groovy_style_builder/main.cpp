e22b89d459e90c71e8837888feea89cfa1b3a51e
crosseax <qq417737572@gmail.com>
2021-12-29 23:40:07 +0800

    setting up

---
+++ builder/groovy_style_builder/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+
+// using uniform initialization syntax
+// to construct a domain specific language 
+// which allows to construct HTML-like constructs
+
+int main (void)
+{
+
+
+    return 0;
+}
