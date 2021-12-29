9e59ba0db48359ee98d6cdbc1a068dc2c5c7c9f9
crosseax <qq417737572@gmail.com>
2021-12-29 23:57:17 +0800

    so far so good, except for the friend function (a slight confusion that acts like a recursion)

--- builder/groovy_style_builder/main.cpp
+++ builder/groovy_style_builder/main.cpp
@@ -11,6 +11,55 @@
 // to construct a domain specific language 
 // which allows to construct HTML-like constructs
 
+struct Tag {
+    std::string name;
+    std::string text;
+    std::vector<Tag> children;
+    std::vector<std::pair<std::string, std::string>> attributes;
+
+    friend std::ostream& operator<< (std::ostream& os, const Tag& tag) {
+        os << "<" << tag.name;
+        
+        for (const auto& att : tag.attributes) {
+            os << " " << att.first << "=\"" << att.second << "\"";
+        }
+
+        if (tag.children.size() == 0 && tag.text.length() == 0) {
+            os << "/>" << std::endl;
+        } else {
+            os << ">" << std::endl;
+
+            if (tag.text.length()) {
+                os << tag.text << std::endl;
+            }
+
+            for (const auto& child : tag.children) {
+                os << child;
+            }
+        }
+
+        return os;
+    }
+
+protected:
+    Tag(const std::string& name, const std::string& text)
+        : name{name}, text{text} {}
+
+    Tag(const std::string& name, const std::vector<Tag>& children)
+        : name{name}, children{children} {}
+
+};
+
+struct P : Tag {
+    P (const std::string& text)
+        : Tag{"p", text} {}
+
+    // when provided as initializing list,
+    // it goes to Tag constructor as vector
+    P(std::initializer_list<Tag> children) 
+        : Tag{"p", children} {}
+};
+
 int main (void)
 {
 
