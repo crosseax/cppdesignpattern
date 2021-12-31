5c0d86fb3c4673965707ac791e787b64250000e3
crosseax <qq417737572@gmail.com>
2021-12-31 11:16:53 +0800

    once again setup for exercise

--- builder/exercise/main.cpp
+++ builder/exercise/main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 class CodeBuilder
 {
 public:
-  CodeBuilder(const string& class_name) {
-      // todo
-  }
+    CodeBuilder(const string& class_name) {
+        // todo
+    }
 
-  CodeBuilder& add_field(const string& name, const string& type) {
-    // todo
-  }
+    CodeBuilder& add_field(const string& name, const string& type) {
+        // todo
+    }
 
-  friend ostream& operator<<(ostream& os, const CodeBuilder& obj) {
-    // todo
-  }
+    friend ostream& operator<<(ostream& os, const CodeBuilder& obj) {
+
+    }
 };
 
 
