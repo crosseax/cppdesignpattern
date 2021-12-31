86682b5e8527d8c55629a780128f300216189bc6
crosseax <qq417737572@gmail.com>
2021-12-31 13:06:14 +0800

    why to use factory

--- factories/point_example/main.cpp
+++ factories/point_example/main.cpp
@@ -6,22 +6,43 @@
 #include <tuple>
 #include <sstream>
 #include <memory>
+#include <cmath>
 
-
+enum class PointType {
+    cartesian,
+    polar
+};
 
 struct Point {
     float x, y;
 
-    Point(float x, float y) : x{x}, y{y} {}
+    // Point(float x, float y) : x{x}, y{y} {}
 
     // at this point it is Cartesian coordinates
     // and what if I want to initiazie it from polar coordinates
 
-    Point(float rho, float theta) {
-        // this wont work cuz the args are same as constructor above, two floats
+    // Point(float rho, float theta) {
+    //     // this wont work cuz the args are same as constructor above, two floats
 
+    // }
+
+    //! \param a this is either x or rho
+    //! \param b this is either y or theta
+    Point (float a, float b, PointType type = PointType::cartesian) {
+        if (type == PointType::cartesian) {
+            x = a;
+            y = b;
+        } else {
+            x = a * cos(b);
+            y = a * sin(b);
+        }
     }
 };
+
+// So far the problem,
+// this is what factory is all about
+// it's about making sure that the interface to a particular type provides is sensible
+// it's understandable, immediately consumable by the client
 
 int main (void)
 {
