e14890d23a6cd6227808a1efae3051af39a3e0c8
crosseax <qq417737572@gmail.com>
2021-12-31 11:11:01 +0800

    setup for exercise

---
+++ builder/exercise/main.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <ostream>
+#include <iostream>
+using namespace std;
+
+class CodeBuilder
+{
+public:
+  CodeBuilder(const string& class_name)
+  {
+      // todo
+  }
+
+  CodeBuilder& add_field(const string& name, const string& type)
+  {
+    // todo
+  }
+
+  friend ostream& operator<<(ostream& os, const CodeBuilder& obj)
+  {
+    // todo
+  }
+};
+
+
+int main (void)
+{
+    auto cb = CodeBuilder{"Person"}.add_field("name", "string").add_field("age", "int");
+    std::cout << cb << std::endl;
+
+    // Expected Output
+    /*
+    class Person {
+        std::string name;
+        int age;
+    };
+    */
+
+    return 0;
+}
