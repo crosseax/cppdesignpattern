18aa66b92350880e108d8da3e99aebdbbe914b90
crosseax <qq417737572@gmail.com>
2022-01-04 22:20:24 +0800

    this is how you do functional factory

--- factories/functional_factory/DrinkFactory.h
+++ factories/functional_factory/DrinkFactory.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "HotDrink.h"
 #include "HotDrinkFactory.h"
+
+#include <functional>
+
 // this is a concrete implementation of the abstract factory
 
 class DrinkFactory {
@@ -16,4 +19,25 @@ public:
         drink->prepare(200);
         return drink;
     }
+};
+
+class DrinkwithVolumeFactory {
+    std::map<std::string, std::function<std::unique_ptr<HotDrink>()>> factories;
+public:
+    DrinkwithVolumeFactory() {
+        factories["tea"] = [] {
+            auto tea = std::make_unique<Tea>();
+            tea->prepare(200);
+            return tea;
+        };
+        // this lambda is the factory method
+        // it's just encapsulated as standalone function
+    }
+
+    std::unique_ptr<HotDrink> make_drink (const std::string& name) {
+        return factories[name]();
+    }
+
+    // now this way you end up with a functional factory 
+    // as opposite to an abstract factory that's based on inheritance
 };
