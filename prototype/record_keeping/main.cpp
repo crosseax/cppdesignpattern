89fce387f041de36715aaec6f832315e45600f38
crosseax <qq417737572@gmail.com>
2022-01-06 11:18:11 +0800

    problem occurs when using pointers

--- prototype/record_keeping/main.cpp
+++ prototype/record_keeping/main.cpp
@@ -15,6 +15,13 @@ struct Address {
 
     Address (const std::string& street, const std::string& city, int suite)
         : street {street}, city {city}, suite{suite} {}
+
+    friend std::ostream& operator<< (std::ostream& os, const Address& address) {
+        os << "Street: " << address.street 
+           << "; City: " << address.city
+           << "; Suite: " << address.suite;
+        return os;
+    }
 };
 
 struct Contact {
@@ -23,15 +30,49 @@ struct Contact {
 
     Contact (const std::string& name, const Address& address) 
         : name {name}, address {address} {}
+
+    friend std::ostream& operator<< (std::ostream& os, const Contact contact) {
+        os << "[Name]: " << contact.name
+           << "\n[Address]: " << contact.address;
+        return os;
+    }
 };
+
+// but in real cases, people usually use pointer instead of strings
+struct Contact2 {
+    std::string name;
+    Address* address;
+
+    Contact (const std::string& name, Address* address) 
+        : name {name}, address {address} {}
+
+    // problems: 
+        // who owns the pointer and who deletes it
+
+    friend std::ostream& operator<< (std::ostream& os, const Contact contact) {
+        os << "[Name]: " << contact.name
+           << "\n[Address]: " << contact.address;
+        return os;
+    }
+};
+
 
 
 int main (void)
 {
     Contact john {"John Doe", Address{"123 East Dr", "London", 123}};
-    Contact jane {"Jane Smith", Address{"123 East Dr", "London", 103}};
+    // Contact jane {"Jane Smith", Address{"123 East Dr", "London", 103}};
     // is it really worth it to keep copying and modifying each time when creating new contact?
 
+    // what you can also do
+    Contact jane = john;
+    jane.name = "Jane Smith";
+    jane.address.suite = 103;
+
+    std::cout << john << std::endl;
+    std::cout << jane << std::endl;
+
+    std::cout << std::endl;
 
     return 0;
 }
