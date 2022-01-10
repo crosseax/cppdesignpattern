2d1edce151674f7cfdf36d4efe894379ed7f13c5
crosseax <qq417737572@gmail.com>
2022-01-10 16:16:07 +0800

    setting up for serialization

--- prototype/prototype_via_serialization/main.cpp
+++ prototype/prototype_via_serialization/main.cpp
@@ -9,6 +9,10 @@
 #include <memory>
 #include <cmath>
 
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
 struct Address {
     std::string street, city;
     int suite;
@@ -39,6 +43,8 @@ struct Contact {
     // we want to save what the pointer points to
     Address* address;
 
+    Contact() = default;
+
     Contact (const std::string& name, Address* address) 
         : name {name}, address {address} {}
 
@@ -57,27 +63,59 @@ struct Contact {
            << "\n[Address]: " << *contact.address;
         return os;
     }
+
+private:
+    // giving boost access of this class
+    friend class boost::serialization::access;
+
+    // you can either spilt the serialization into the saving and loading if you want the logic to be different
+    // or you can just have a single function which kind of takes care of both of these
+    template <class archive>
+    void serialize(archive& ar, const unsigned version) {
+        ar & name; // using the ampersand operator& to define the parts we serialize
+        ar & address; // we dont have to dereference the pointer, it knows if it's a pointer, we have to follow it to serialize what we interested in
+    }
 };
+
+
+
 
 
 int main (void)
 {
-    Contact john {"John Doe", new Address{"123 East Dr", "London", 123}};
-    // so here, john is acting like a prototype,
-    // and we replicate the prototype using copy constructor
-    // then customize the copied instance
+    // Contact john {"John Doe", new Address{"123 East Dr", "London", 123}};
+    // // so here, john is acting like a prototype,
+    // // and we replicate the prototype using copy constructor
+    // // then customize the copied instance
     
-    Contact jane{john};
-    // or Contact jane = john;
+    // Contact jane{john};
+    // // or Contact jane = john;
     
 
-    jane.name = "Jane Smith";
-    jane.address->suite = 103;
+    // jane.name = "Jane Smith";
+    // jane.address->suite = 103;
 
-    std::cout << john << std::endl;
-    std::cout << jane << std::endl;
-    std::cout << "Issue: Outputing memory address instead of values" << std::endl;
-    std::cout << "Issue: Changing the Copy also changes the original" << std::endl;
+    // std::cout << john << std::endl;
+    // std::cout << jane << std::endl;
+    // std::cout << "Issue: Outputing memory address instead of values" << std::endl;
+    // std::cout << "Issue: Changing the Copy also changes the original" << std::endl;
+
+    auto clone = [](const Contact& c) {
+        // first serialize it to string
+        std::ostringstream oss;
+        boost::archive::text_oarchive oa(oss);
+        oa << c;
+        std::string s = oss.str();
+        std::cout << s << std::endl;
+
+        // then deserialize it into a new object
+        std::istringstream iss(s);
+        boost::archive::text_iarchive ia(iss);
+        Contact result;
+        ia >> result;
+        return result;
+    };
+
 
     return 0;
 }
