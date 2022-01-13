a24a9ef8fcdfdd87338cecb7fa4e905d2b204d08
crosseax <qq417737572@gmail.com>
2022-01-13 15:34:12 +0800

    how is the initializing database between city and population after being compiled, how to solve this?

--- singleton/singleton_implementation/main.cpp
+++ singleton/singleton_implementation/main.cpp
@@ -1,4 +1,5 @@
 #define _USE_MATH_DEFINES
+#define _HAS_AUTO_PTR_ETC 1
 #include <iostream>
 #include <cstdio>
 #include <string>
@@ -8,14 +9,52 @@
 #include <sstream>
 #include <memory>
 #include <cmath>
+#include <map>
 
+#include <boost/lexical_cast.hpp>
 
+// we wanna read the txt file to database
+// for us, it only make sense to have one instance of such database
+// because you dont want to replicate the information again and again
+// So, we're going to make a singleton database effectively
+class SingletonDatabase {
+    SingletonDatabase() {
+        std::cout << "Initializing database\n";
+        std::ifstream ifs ("capitals.txt");
 
+        std::string s, s2;
+
+        while (getline(ifs, s)) {
+            getline(ifs, s2);
+            int population = boost::lexical_cast<int>(s2);
+            capitals[s] = population;
+        }
+    }
+    std::map<std::string, int> capitals;
+public:
+    // those delete ensures that nobody can actually replicate this database in anyway
+    // thereby wasting database resources
+    SingletonDatabase(SingletonDatabase const&) = delete;
+    void operator= (SingletonDatabase const&) = delete;
+
+    static SingletonDatabase& get() {
+        static SingletonDatabase db;
+        return db;
+    }
+
+    int get_population(const std::string& name) {
+        return capitals[name];
+    }
+};
 
 int main (void)
 {
-
-
+    std::string city = "Tokyo";
+    std::cout << city << " has population " <<
+        SingletonDatabase::get().get_population(city) << std::endl;
+    
+    // // Not allowed, because the copy constructor is deleted
+    // auto db = SingletonDatabase::get();
 
     return 0;
 }
