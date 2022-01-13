d91ee092d41e263f6aa1f13dc2c948d4489c65e7
crosseax <qq417737572@gmail.com>
2022-01-13 15:15:05 +0800

    lets go

---
+++ singleton/singleton_implementation/main.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+#include <cmath>
+
+
+
+
+int main (void)
+{
+
+
+
+    return 0;
+}
